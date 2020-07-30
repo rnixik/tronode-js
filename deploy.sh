a0668274243f1b887bef93d99e013a9fcd0efd61
Roman Nix <rnix@yandex.ru>
2020-07-30 16:57:00 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-#
-# deploy project
-#
-#
 
-TARG="/var/www/tronode.js"
-HGSOURCE="ssh://hg@bitbucket.org/rnix/tronode.js"
-
-cd "$TARG" || exit 1
-
-echo hg pull -u "$HGSOURCE"
-hg pull -u "$HGSOURCE"
-
-forever restart app.js
-
-echo OK!
-
+git pull && docker-compose up -d --build
